675c94e1c0dcd26edb66374b1e2608cf0f8dd4de
skylerputney <121919826+skylerputney@users.noreply.github.com>
2025-03-28 11:44:37 -0400

    LW.c

--- LW.c
+++ LW.c
@@ -1,71 +1,100 @@
 #include "Instruction.h"
 
 void lw_immd_assm(void) {
+	// Checking op code matches
 	if (strcmp(OP_CODE, "LW") != 0) {
-
+		// Doesn't match - incorrect command
 		state = WRONG_COMMAND;
 		return;
 	}
 	
+	/*
+		Checking parameter types
+	*/
 	
+	// First parameter is register (Rt)
 	if (PARAM1.type != REGISTER) {
 		state = MISSING_REG;
 		return;
 	}
 
+	// Second parameter is immediate (offset)
 	if (PARAM2.type != IMMEDIATE) {
 		state = INVALID_IMMED;
 		return;
 	}
 
-	//reg
+	// Third parameter is register (base address, Rs)
 	if (PARAM3.type != REGISTER) {
 		state = MISSING_REG;
 		return;
 	}
 
-	//check if register 1 and 3
+	/*
+		Checking parameter values
+	*/
+
+	// Register should be 31 or less
 	if (PARAM1.value > 31) {
 		state = INVALID_REG;
 		return;
 	}
+
+	// Register should be 31 or less
 	if ( PARAM3.value > 31) {
 		state = INVALID_REG;
 		return;
 	}
-	//register 2 
-	if ( PARAM2.value > 0x7FFF) {
+
+	// The immediate value is limited to 16 bits, this is 0xFFFF
+	if ( PARAM2.value > 0xFFFF) {
 		state = INVALID_IMMED;
 		return;
 	}
 	
-	//encode instruction 
+	/*
+		Assembling the binary
+	*/
+
+	// Set opcode
 	setBits_str(31, "100011");
+	// Set Rt
 	setBits_num(20, PARAM1.value, 5);
+	// Set Rs (base)
 	setBits_num(25, PARAM3.value, 5);
+	// Set immediate (offset)
 	setBits_num(15, PARAM2.value, 16);
 
-
+	// Signal encoding done
 	state = COMPLETE_ENCODE;
 }
 
 
 void lw_immd_bin(void) {
-
-	//check op code 
+	// Check op code bits match
 	if (checkBits(31, "100011") != 0) {
 		state = WRONG_COMMAND;
 		return;
 	}
 
+	/*
+		Obtain values from binary
+	*/
+
 	uint32_t Rs = getBits(25, 5);
 	uint32_t Rt = getBits(20, 5);
 	uint32_t imm16 = getBits(15, 16);
+
+	/*
+		Setting Instruction values
+	*/
 
 	setOp("LW");
 	setParam(1, REGISTER, Rt);
 	setParam(3, REGISTER, Rs);
 	setParam(2, IMMEDIATE, imm16);
+
+	// Signal decoding done
 	state = COMPLETE_DECODE;
 
 }
