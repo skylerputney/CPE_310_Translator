f9b9a03acda32fbdf883904cb2fa7befdd47879f
Alex Patterson <acp00011@mix.wvu.edu>
2025-04-02 21:28:50 -0400

    LUI.c This instruction only uses two params so I got rid of param 3, if it gives weird register or param related errors that's why

--- LUI.c
+++ LUI.c
@@ -9,62 +9,87 @@
 #include "Instruction.h"
 
 void lui_immd_assm(void) {
+	// Check if the OPCODE matches LUI
 	if (strcmp(OP_CODE, "LUI") != 0) {
+		// If the opcode is not LUI then the state is wrong command
 		state = WRONG_COMMAND;
 		return;
 	}
 
+	/*
+		Parameter checking
+	*/
+
+	// First param should be register
 	if (PARAM1.type != REGISTER) {
 		state = MISSING_REG;
 		return;
 	}
-	if (PARAM2.type != REGISTER) {
-		state = MISSING_REG;
-		return;
-	}
-	if (PARAM3.type != IMMEDIATE) {
+	
+	// Second param should be immediate
+	if (PARAM2.type != IMMEDIATE) {
 		state = INVALID_PARAM;
 		return;
 	}
 
+	/*
+		Checking param values
+	*/
+
+	// Rt should be less than or equal to 31
 	if (PARAM1.value > 31) {
 		state = INVALID_REG;
 		return;
 	}
-	if (PARAM2.value > 31) {
-		state = INVALID_REG;
-		return;
-	}
-	if (PARAM3.value > 0xFFFF) {
+
+	// imm should be less than or equal to 0xFFFF
+	if (PARAM2.value > 0xFFFF) {
 		state = INVALID_IMMED;
 		return;
 	}
 
-	setBits_str(31, "001111");
-	setBits_num(20, PARAM1.value, 5);
-	setBits_num(25, PARAM2.value, 5);
-	setBits_num(15, PARAM3.value, 16);
+	/*
+		Putting binary together
+	*/
 
+	// Setting opcode
+	setBits_str(31, "001111");
+
+	// Setting dont care bits
+	setBits_str(25, "00000");
+
+	// Setting rt value
+	setBits_num(20, PARAM1.value, 5);
+
+	// Setting imm value
+	setBits_num(15, PARAM2.value, 16);
+
+	// Set system state to complete encode
 	state = COMPLETE_ENCODE;
 }
 
 void lui_immd_bin(void) {
+	// Check if opcode bits are correct
 	if (checkBits(31, "001111") != 0) {
 		state = WRONG_COMMAND;
 		return;
 	}
 
-	uint32_t Rs = getBits(25, 5);
+	/*
+		Finding values in the machine code
+	*/
+
 	uint32_t Rt = getBits(20, 5);
 	uint32_t imm16 = getBits(15, 16);
 
-	setOp("LUI");
-	setParam(1, REGISTER, Rt); 
-	setParam(2, REGISTER, Rs); 
-	setParam(3, IMMEDIATE, imm16); 
+	/*
+		Setting instruction values
+	*/
 
+	setOp("LUI");
+	setParam(1, REGISTER, Rt); // register param
+	setParam(2, IMMEDIATE, imm16); // immediate param
+
+	// Set system state to complete decode
 	state = COMPLETE_DECODE;
 }
-
-
-
