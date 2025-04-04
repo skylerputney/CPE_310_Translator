37b5871f789d8e9fcd660ecb28d68a0bf7efea36
Alex Patterson <acp00011@mix.wvu.edu>
2025-04-04 12:55:05 -0400

    SLTI.c Implemented STLI, it is very similar to ORI

--- SLTI.c
+++ SLTI.c
@@ -10,11 +10,102 @@
 #include "Instruction.h"
 
 void slti_immd_assm(void) {
-	state = WRONG_COMMAND;
+	// check if opcode matches
+	if (strcmp(OP_CODE, "SLTI") != 0){
+		state = WRONG_COMMAND;
+		return;
+	}
+
+	/*
+		Checking param types
+	*/
+
+	// param1 should be register (rt)
+	if (PARAM1.type != REGISTER) {
+		state = MISSING_REG;
+		return;
+	}
+
+	// param2 should be register (rs)
+	if (PARAM2.type != REGISTER) {
+		state = MISSING_REG;
+		return;
+	}
+
+	// param3 should be immediate
+	if (PARAM3.type != IMMEDIATE) {
+		state = INVALID_PARAM;
+		return;
+	}
+
+	/*
+		Checking the value of parameters
+	*/
+
+	// rt must be <= 31
+	if (PARAM1.value > 31) {
+		state = INVALID_REG;
+		return;
+	}
+
+	// rs must be <= 31
+	if (PARAM2.value > 31) {
+		state = INVALID_REG;
+		return;
+	}
+
+	// The immediate must be <= 0xFFFF
+	if (PARAM3.value > 0xFFFF) {
+		state = INVALID_IMMED;
+		return;
+	}
+
+	/*
+		Putting binary together
+	*/
+
+	// setting opcode
+	setBits_str(31, "001010");
+
+	// setting rs
+	setBits_num(25, PARAM2.value, 5);
+
+	// setting rt
+	setBits_num(20, PARAM1.value, 5);
+
+	// setting imm
+	setBits_num(15, PARAM3.value, 16);
+
+	// set system state to complete encode
+	state = COMPLETE_ENCODE;
 }
 
 void slti_immd_bin(void) {
-	state = WRONG_COMMAND;
+	// make sure opcode matches
+	if(checkBits(31, "001010") != 0){
+		state = WRONG_COMMAND;
+		return;
+	}
+
+	/*
+		find values within binary
+	*/
+
+	uint32_t Rs = getBits(25, 5);
+	uint32_t Rt = getBits(20, 5);
+	uint32_t imm16 = getBits(15, 16);
+
+	/*
+		set instruction values
+	*/
+
+	setOp("SLTI");
+	setParam(1, REGISTER, Rt); // register to be set
+	setParam(2, REGISTER, Rs); // register to be compared to imm
+	setParam(3, IMMEDIATE, imm16); // imm used for comp
+
+	// set system state to complete decode
+	state = COMPLETE_DECODE;
 }
 
 
